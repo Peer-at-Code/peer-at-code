85ac0a6b1922052cc7ce5a32d57a4c3b1b328aa7
Théo <43091603+glazk0@users.noreply.github.com>
2023-02-10 17:26:05 +0100

    Format

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,17 +6,17 @@ export default function Page() {
     <div className="flex h-screen w-full">
       <div className="">
         <h1 className="text-2xl font-bold tracking-tight">Statistique</h1>
-        <div className="flex flex-col space-x-0 md:flex-row md:space-x-9 pt-5">
+        <div className="flex flex-col space-x-0 pt-5 md:flex-row md:space-x-9">
           <Card />
           <Card />
           <Card />
         </div>
-        <div className="flex flex-col space-x-0 md:flex-row md:space-x-[5%] pt-5 h-[60%]">
-          <div className='w-6/12 mb-9'>
+        <div className="flex h-[60%] flex-col space-x-0 pt-5 md:flex-row md:space-x-[5%]">
+          <div className="mb-9 w-6/12">
             <h1 className="text-2xl font-bold tracking-tight">Guide</h1>
-            <div className="flex flex-col space-x-0 md:flex-row md:space-x-[10%] pt-5 h-full ">
-              <div className="bg-dark rounded-lg shadow-md w-full h-full">
-                <div className="flex py-2 px-4 rounded-lg justify-between">
+            <div className="flex h-full flex-col space-x-0 pt-5 md:flex-row md:space-x-[10%] ">
+              <div className="h-full w-full rounded-lg bg-dark shadow-md">
+                <div className="flex justify-between rounded-lg py-2 px-4">
                   <div className="my-auto">
                     <p className="font-bold">Bold data</p>
                     <p className="text-lg">Simple data</p>
@@ -25,9 +25,9 @@ export default function Page() {
               </div>
             </div>
           </div>
-          <div className='w-6/12 mb-9'>
+          <div className="mb-9 w-6/12">
             <h1 className="text-2xl font-bold tracking-tight">Dernières activités </h1>
-            <div className="flex flex-col space-x-0 md:flex-row md:space-x-[10%] pt-5 h-full">
+            <div className="flex h-full flex-col space-x-0 pt-5 md:flex-row md:space-x-[10%]">
               <CardTable />
             </div>
           </div>
@@ -36,7 +36,3 @@ export default function Page() {
     </div>
   );
 }
-
-
-
-
