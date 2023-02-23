8f4085cd9b6301529ad3925ba11c006312346512
Nicolas VINCENT <ni.vincent@student.helmo.be>
2023-02-23 14:01:05 +0100

    update/home-page
    ajout d'une description à la page à accueille

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< Updated upstream
   reactStrictMode: false, // Recommended for the `pages` directory, default in `app`.
+=======
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+>>>>>>> Stashed changes
   swcMinify: true,
   experimental: {
     appDir: true
