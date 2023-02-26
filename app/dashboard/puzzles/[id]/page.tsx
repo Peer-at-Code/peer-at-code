aadbf4bcbcf2b0f49d16d8ea0c74069ce80fb480
Théo <43091603+glazk0@users.noreply.github.com>
2023-02-26 19:23:51 +0100

    Fix static issue

--- app/dashboard/puzzles/[id]/page.tsx
+++ app/dashboard/puzzles/[id]/page.tsx
@@ -1,13 +1,10 @@
-import { getPuzzle, getPuzzles } from '@/lib/puzzles';
+import { getPuzzle } from '@/lib/puzzles';
 import Puzzle from '@/ui/Puzzle';
 import { notFound } from 'next/navigation';
 
-export async function generateMetadata({
-  params
-}: {
-  params: { id: number };
-  searchParams: URLSearchParams;
-}) {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export async function generateMetadata({ params }: { params: { id: number } }) {
   const { id } = params;
 
   const puzzle = await getPuzzle(id);
@@ -27,8 +24,8 @@ export default async function Page({ params }: { params: { id: number } }) {
   return <Puzzle puzzle={puzzle} />;
 }
 
-export async function generateStaticParams() {
-  const { puzzles } = await getPuzzles();
-  // every id is a number, but we need to return a string
-  return puzzles.map((puzzle) => ({ id: puzzle.id.toString() }));
-}
+// export async function generateStaticParams() {
+//   const { puzzles } = await getPuzzles();
+//   // every id is a number, but we need to return a string
+//   return puzzles.map((puzzle) => ({ id: puzzle.id.toString() }));
+// }
