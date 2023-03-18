bd8418b86a0a2f26cd7448b5e3b75fefcf6d994f
Théo <43091603+glazk0@users.noreply.github.com>
2023-03-18 17:31:42 +0100

    Created respective hooks

--- lib/hooks/use-puzzles.ts
+++ lib/hooks/use-puzzles.ts
@@ -2,14 +2,14 @@ import useSWR from 'swr';
 
 import { getChapters, getPuzzle, getPuzzles } from '../puzzles';
 
-export function useChapters() {
-  return useSWR('chapters', () => getChapters());
+export function useChapters({ token }: { token: string }) {
+  return useSWR('chapters', () => getChapters({ token }));
 }
 
-export function usePuzzles() {
-  return useSWR('puzzles', () => getPuzzles());
+export function usePuzzles({ token }: { token: string }) {
+  return useSWR('puzzles', () => getPuzzles({ token }));
 }
 
-export function usePuzzle(id: number) {
-  return useSWR(`puzzles/${id}`, () => getPuzzle(id));
+export function usePuzzle({ token, id }: { token: string; id: number }) {
+  return useSWR(`puzzles/${id}`, () => getPuzzle({ token, id }));
 }
